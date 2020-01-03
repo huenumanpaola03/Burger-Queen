b2d5c0732f66fe7f93d9494e5f1b1fdce35c5c47
huenumanpaola03 <huenumanpaola03@gmail.com>
2020-01-03 10:09:39 -0300

    subiendo para poder bajar no aceptar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import logo from './logo.png'; 
+
+
 
 import './App.css';
+
 import Organism from './Components/organisms/Organism';
 class App extends Component {
   render(){
     return <div>
       <Organism />
+    <div>
+ 
+</div>
+
+
     </div>
   }
 }
